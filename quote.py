3650266453bbb0c6641a878c2465a7e4cfb4b842
Will Soula <william.soula@starz.com>
2021-05-13 11:10:33 -0600

    Convert to json

--- quote.py
+++ quote.py
@@ -1,5 +1,6 @@
 from errbot import BotPlugin, botcmd, arg_botcmd, re_botcmd
 import urllib.request
+import json
 
 class Quote(BotPlugin):
     """Return random quotes"""
@@ -8,5 +9,5 @@ class Quote(BotPlugin):
     def quote(self, msg, args):
         url = 'https://zenquotes.io/api/random'
         page = urllib.request.Request(url)
-        response = urllib.request.urlopen(page).read().decode('utf-8')
+        response = json.loads(urllib.request.urlopen(page).read().decode('utf-8'))
         return response[0]['q']+'\n- '+response[0]['a']
